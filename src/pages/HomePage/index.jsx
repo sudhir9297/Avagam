5c7ed496cb51a9d08d896b728acedd24164a2d99
sudhir <sudhir9297@gmail.com>
2022-08-13 16:30:25 +0530

    added media query

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -7,13 +7,15 @@ function HomePage() {
     {
       id: 1,
       name: "Plant World",
-      image: "https://i.imgur.com/qkKy8.jpg",
+      image:
+        "https://default-vessel.s3.ap-south-1.amazonaws.com/avagam-bucket/plant-world.png",
       link: "plant-world",
     },
     {
       id: 2,
-      name: "working",
-      image: "https://i.imgur.com/qkKy8.jpg",
+      name: "working...",
+      image:
+        "https://aakaar.s3.ap-south-1.amazonaws.com/textures/Default/default-image.jpg",
       link: "plant-world",
     },
   ];
@@ -23,7 +25,10 @@ function HomePage() {
       <Container>
         {List.map((o) => (
           <Link to={`/project/${o.link}`} key={o.id}>
-            <CardBox key={o.id}>{o.name}</CardBox>
+            <CardBox key={o.id}>
+              <Image src={o.image} />
+              <Text>{o.name}</Text>
+            </CardBox>
           </Link>
         ))}
       </Container>
@@ -44,11 +49,16 @@ const Container = styled.div`
   gap: 2.2rem;
   grid-auto-flow: dense;
   padding: 2rem;
+
+  a {
+    color: black;
+    text-decoration: none;
+  }
 `;
 
 const CardBox = styled.div`
   background: #fff;
-  padding: 2rem;
+  padding: 0.4rem;
   height: 140px;
   display: flex;
   align-items: center;
@@ -58,5 +68,20 @@ const CardBox = styled.div`
   box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
     rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
 
-  /* box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px; */
+  display: flex;
+  flex-direction: column;
+  height: fit-content;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 3px;
+`;
+
+const Text = styled.div`
+  width: 100%;
+  font-size: 12px;
+  text-decoration: none;
+  margin-top: 10px;
 `;
--- src/pages/Project/PlantWorld/index.jsx
+++ src/pages/Project/PlantWorld/index.jsx
@@ -1,66 +1,100 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled, { css } from "styled-components";
 import { a, useSpring } from "react-spring";
 import { useSpringCarousel } from "react-spring-carousel";
 import { device } from "../../../constant";
+import { Link } from "react-router-dom";
 
 function Carousel() {
   const [active, setActive] = useState(1);
+  const [noOfItem, setNoOfItem] = useState(6);
+
+  const handleSize = (innerWidth) => {
+    if (innerWidth < 599) {
+      return setNoOfItem(3);
+    }
+    if (innerWidth < 899) {
+      return setNoOfItem(3);
+    }
+    if (innerWidth < 1199) {
+      return setNoOfItem(4);
+    }
+    return setNoOfItem(6);
+  };
+
+  useEffect(() => {
+    handleSize(window.innerWidth);
+
+    window.addEventListener("resize", (e) => handleSize(e.target.innerWidth));
+    return () => {
+      window.removeEventListener("resize", (e) =>
+        handleSize(e.target.innerWidth)
+      );
+    };
+  }, []);
+
   const List = [
     {
       id: 1,
       title: "Dracaena Trifasciata",
-      image: "/assets/Images/4.png",
+      image:
+        "https://default-vessel.s3.ap-south-1.amazonaws.com/avagam-bucket/4.png",
       desc: "One of the most effective houseplants in air purification.",
     },
-
     {
       id: 2,
       title: "Crassula Ovata",
-      image: "/assets/Images/3.png",
+      image:
+        "https://default-vessel.s3.ap-south-1.amazonaws.com/avagam-bucket/3.png",
       desc: "One of the most effective houseplants in air purification.",
     },
     {
       id: 3,
       title: "Browningia Hertlingiana",
-      image: "/assets/Images/1.png",
+      image:
+        "https://default-vessel.s3.ap-south-1.amazonaws.com/avagam-bucket/1.png",
       desc: "One of the most effective houseplants in air purification.",
     },
     {
       id: 4,
       title: "Haworthiopsis Attenuata ",
-      image: "/assets/Images/5.png",
+      image:
+        "https://default-vessel.s3.ap-south-1.amazonaws.com/avagam-bucket/5.png",
       desc: "One of the most effective houseplants in air purification.",
     },
     {
       id: 5,
       title: "Chlorophytum Comosum",
-      image: "/assets/Images/2.png",
+      image:
+        "https://default-vessel.s3.ap-south-1.amazonaws.com/avagam-bucket/2.png",
       desc: "One of the most effective houseplants in air purification.",
     },
     {
       id: 6,
       title: "Dracaena Trifasciata",
-      image: "/assets/Images/4.png",
+      image:
+        "https://default-vessel.s3.ap-south-1.amazonaws.com/avagam-bucket/4.png",
       desc: "One of the most effective houseplants in air purification.",
     },
-
     {
       id: 7,
       title: "Crassula Ovata",
-      image: "/assets/Images/3.png",
+      image:
+        "https://default-vessel.s3.ap-south-1.amazonaws.com/avagam-bucket/3.png",
       desc: "One of the most effective houseplants in air purification.",
     },
     {
       id: 8,
       title: "Browningia Hertlingiana",
-      image: "/assets/Images/1.png",
+      image:
+        "https://default-vessel.s3.ap-south-1.amazonaws.com/avagam-bucket/1.png",
       desc: "One of the most effective houseplants in air purification.",
     },
     {
       id: 9,
       title: "Haworthiopsis Attenuata ",
-      image: "/assets/Images/5.png",
+      image:
+        "https://default-vessel.s3.ap-south-1.amazonaws.com/avagam-bucket/5.png",
       desc: "One of the most effective houseplants in air purification.",
     },
   ];
@@ -71,7 +105,7 @@ function Carousel() {
     carouselFragment,
     useListenToCustomEvent,
   } = useSpringCarousel({
-    itemsPerSlide: 6,
+    itemsPerSlide: noOfItem,
     gutter: 30,
     withLoop: true,
     startEndGutter: 100,
@@ -108,6 +142,11 @@ function Carousel() {
   return (
     <Container>
       <HeaderWrapper>
+        <OptionWrapper>
+          <Link to="/">
+            <Option>Go Back</Option>
+          </Link>
+        </OptionWrapper>
         <OptionWrapper>
           <Option>Home</Option>
           <Option>Plant Finder</Option>
@@ -153,17 +192,6 @@ const Container = styled.div`
   align-items: center;
   background-color: #d9e5e4;
   overflow: hidden;
-  
-  /* @media ${device.forMobileOnly} {
-    margin: 0;
-    justify-content: flex-start;
-    height: fit-content;
-  }
-  @media ${device.forTabletPortraitOnly} {
-    margin: 0;
-    justify-content: flex-start;
-    height: fit-content;
-  } */
 `;
 
 const wrapperCommon = css`
@@ -180,36 +208,89 @@ const innerWrapperCommon = css`
 const HeaderWrapper = styled.div`
   ${wrapperCommon}
   height: 60px;
+
+  @media ${device.forMobileOnly} {
+    display: none;
+  }
 `;
 
 const OptionWrapper = styled.div`
   ${innerWrapperCommon}
   display: flex;
   align-items: center;
+
+  a {
+    color: #1a2e35;
+    text-decoration: none;
+  }
 `;
 
 const Option = styled.div`
-  margin-right: 2rem;
-  padding-right: 2rem;
-
+  margin: 1rem;
+  padding: 1rem;
+  color: #1a2e35;
+  white-space: nowrap;
   :hover {
     cursor: pointer;
     color: #5e9270;
+  }
+
+  @media ${device.forTabletLandscapeOnly} {
+    padding: 0.6rem;
+  }
+
+  @media ${device.forTabletPortraitOnly} {
+    padding: 0rem;
+    margin: 0.6rem;
   }
 `;
 
 const FirstWrapper = styled.div`
   ${wrapperCommon}
   padding-top: 6rem;
+
+  @media ${device.forMobileOnly} {
+    padding-top: 2rem;
+    padding-bottom: 3rem;
+  }
+
+  @media ${device.forTabletPortraitOnly} {
+    padding-top: 4rem;
+    padding-bottom: 3rem;
+  }
+
+  @media ${device.forTabletLandscapeOnly} {
+    padding: 2rem 0;
+  }
 `;
 
 const InnerWrapper = styled.div`
   ${innerWrapperCommon}
+
+  @media ${device.forMobileOnly} {
+    width: 100%;
+    padding: 0 10px;
+  }
+
+  @media ${device.forTabletPortraitOnly} {
+    width: 60%;
+    padding: 0 10px;
+
+  }
+  @media ${device.forTabletLandscapeOnly} {
+    width: 60%;
+    padding: 0 10px;
+  }
+  
 `;
 
 const Headtext = styled.div`
   font-size: 25px;
   color: #5e9270;
+
+  @media ${device.forMobileOnly} {
+    font-size: 20px;
+  }
 `;
 
 const TextWrapper = styled.div`
@@ -218,6 +299,25 @@ const TextWrapper = styled.div`
   align-items: center;
   padding: 2rem 0;
   margin-bottom: 30px;
+
+  @media ${device.forMobileOnly} {
+    margin-bottom: 0px;
+    width: 100%;
+    padding: 0rem 0px;
+    flex-direction: column;
+  }
+
+  @media ${device.forTabletPortraitOnly} {
+    width: 100%;
+    padding: 0rem 0px;
+    flex-direction: column;
+  }
+
+  @media ${device.forTabletLandscapeOnly} {
+    margin-bottom: 0px;
+    width: 100%;
+    padding: 2rem 0px;
+  }
 `;
 
 const BoldText = styled.div`
@@ -227,12 +327,47 @@ const BoldText = styled.div`
   width: 30%;
   margin-right: 40px;
   line-height: 80px;
+
+  @media ${device.forMobileOnly} {
+    width: 100%;
+    margin-right: 0px;
+    font-size: 40px;
+    line-height: 60px;
+  }
+
+  @media ${device.forTabletPortraitOnly} {
+    width: 100%;
+    margin-right: 0px;
+    font-size: 40px;
+    line-height: 60px;
+  }
+
+  @media ${device.forTabletLandscapeOnly} {
+    margin-right: 0px;
+    font-size: 40px;
+    line-height: 50px;
+  }
 `;
 
 const Text = styled.div`
   width: 30%;
   padding: 10px;
   font-size: 16px;
+
+  @media ${device.forMobileOnly} {
+    width: 100%;
+    padding: 10px 0;
+  }
+
+  @media ${device.forTabletPortraitOnly} {
+    width: 100%;
+    padding: 10px 0;
+  }
+
+  @media ${device.forTabletLandscapeOnly} {
+    width: 50%;
+    padding: 10px 0;
+  }
 `;
 
 const SliderWrapper = styled.div`
@@ -241,6 +376,15 @@ const SliderWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @media ${device.forMobileOnly} {
+    height: fit-content;
+    padding: 50px 0;
+  }
+
+  @media ${device.forTabletPortraitOnly} {
+    margin-top: 1rem;
+  }
 `;
 
 const SliderContainer = styled.div`
@@ -259,6 +403,16 @@ const CardBoxWrapper = styled.div`
   transition: all 0.3s ease-in-out;
   transform-origin: bottom;
   transform: ${({ isActive }) => (isActive ? "scale(1.2)" : "scale(0.9)")};
+
+  @media ${device.forMobileOnly} {
+    width: 200px;
+    height: 300px;
+  }
+
+  @media ${device.forTabletPortraitOnly} {
+    width: 200px;
+    height: 300px;
+  }
 `;
 
 const BackgroundCard = styled.div`
@@ -271,8 +425,17 @@ const BackgroundCard = styled.div`
 
   border-radius: 15px;
 
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
-    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 50px;
+
+  @media ${device.forMobileOnly} {
+    width: 100%;
+    height: 70%;
+  }
+
+  @media ${device.forTabletPortraitOnly} {
+    width: 100%;
+    height: 70%;
+  }
 `;
 
 const Image = styled.img`
@@ -285,6 +448,30 @@ const Image = styled.img`
   transform-origin: bottom;
   transform: ${({ isActive }) =>
     isActive ? "scale(1.2) translateY(-40px)" : "scale(1) translateY(0px)"};
+
+  @media ${device.forMobileOnly} {
+    width: 180px;
+    height: auto;
+
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  @media ${device.forTabletPortraitOnly} {
+    width: 220px;
+    height: auto;
+    left: 50%;
+    bottom: 10%;
+    transform: translate(-50%, 0%);
+  }
+
+  @media ${device.forTabletLandscapeOnly} {
+    width: 250px;
+    left: 50%;
+    bottom: 10%;
+    transform: translate(-50%, 0%);
+  }
 `;
 
 const ItemText = styled.div`
@@ -295,13 +482,24 @@ const ItemText = styled.div`
   flex-direction: column;
   width: 80%;
 
+  @media ${device.forMobileOnly} {
+    bottom: 10%;
+  }
+
   h3 {
     font-weight: 600;
     font-size: 18px;
+    @media ${device.forMobileOnly} {
+      font-size: 16px;
+    }
   }
 
   div {
     display: ${({ isActive }) => (isActive ? "block" : "none")};
     font-size: 12px;
+
+    @media ${device.forMobileOnly} {
+      font-size: 10px;
+    }
   }
 `;
